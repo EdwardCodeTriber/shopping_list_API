a97cb2cb0f2a59fcdd8fcf12a06005f7c9aa3d4a
Thapelo Somo <edwardthapelo55@gmail.com>
2024-10-22 16:30:08 +0200

    modified end-point and items

--- shopping_List/controllers/itemController.js
+++ shopping_List/controllers/itemController.js
@@ -1,8 +1,8 @@
-import items from "../models/items.js";
+import Items from "../models/items.js";
 
 const createItem = async (req, res) =>{
     try {
-        const item = await items.create(req.body)
+        const item = await Items.create(req.body)
         res.status(201).json(item)
     } catch (error) {
         res.status(500).json({error: "An error occured while creating the Item"})
--- shopping_List/server.js
+++ shopping_List/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-app.use("items", router)
+app.use("/api/v1", router)
 // MongoDB connection with the .env file
 connectDB()
 
